645da31549cda4a1c83079ee29e1617f268db4fa
Davide Pulci <davide.pulci@gmail.com>
2022-04-06 17:18:51 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,16 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,41 @@
 <template>
-  <header></header>
+  <header>
+    <img src="../assets/dc-logo.png" alt="Logo" />
+    <div class="header-menu">
+      <ul>
+        <li>
+          <a href="#"> <strong>CHARACTERS</strong> </a>
+        </li>
+        <li>
+          <a href="#"><strong> COMICS</strong></a>
+        </li>
+        <li>
+          <a href="#"><strong> MOVIES</strong></a>
+        </li>
+        <li>
+          <a href="#"><strong> TV</strong></a>
+        </li>
+        <li>
+          <a href="#"><strong> GAMES</strong></a>
+        </li>
+        <li>
+          <a href="#"><strong>COLLECTIBLES</strong> </a>
+        </li>
+        <li>
+          <a href="#"><strong>VIDEOS</strong> </a>
+        </li>
+        <li>
+          <a href="#"><strong>FANS</strong> </a>
+        </li>
+        <li>
+          <a href="#"><strong>NEWS</strong> </a>
+        </li>
+        <li>
+          <a href="#"><strong>SHOP</strong> </a>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -10,4 +46,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px;
+
+  img {
+    width: 80px;
+  }
+
+  .header-menu {
+    display: flex;
+    align-items: center;
+
+    ul {
+      display: flex;
+
+      li {
+        list-style-type: none;
+
+        a {
+          text-decoration: none;
+          color: black;
+          margin: 10px;
+          font-size: 14px;
+
+          &:hover {
+            color: blue;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
