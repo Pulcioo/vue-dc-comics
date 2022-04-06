48bb8f32174dad553cc441843702880d73081ef6
Davide Pulci <davide.pulci@gmail.com>
2022-04-06 15:38:59 +0200

    aggiunte le tre macroaree

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "mainComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
